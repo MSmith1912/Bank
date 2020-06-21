ea71a9ab56541073d36ea0aa8c06b1a45f6c01d2
MarkS <mark.smith1@fdmgroup.com>
2020-06-21 18:43:06 +0100

    Bank

---
+++ src/main/java/com/fdmgroup/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+	//can you see me
+}
